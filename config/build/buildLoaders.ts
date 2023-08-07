50d5064791a320d49ebd74ab4cce2ade823c0fac
Filipp Kadushkin <kadushkin.feniks@gmail.com>
2023-08-08 01:57:53 +0300

    feat: add svgLoader and fileLoader. fix navigation dashboard (add button component)

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -4,6 +4,11 @@ import { BuildOptions } from "./types/config";
 
 export function buildLoaders({isDev}: BuildOptions): webpack.RuleSetRule[] {
 
+    const svgLoader = {
+        test: /\.svg$/,
+        use: ['@svgr/webpack'],
+    }
+
     const cssLoader = {
         test: /\.s[ac]ss$/i,
         use: [
@@ -31,7 +36,19 @@ export function buildLoaders({isDev}: BuildOptions): webpack.RuleSetRule[] {
         test: /\.tsx?$/,
         use: 'ts-loader',
         exclude: /node_modules/,}
+
+const fileLoader = {
+    test: /\.(png|jpe?g|gif|woff2|woff)$/i,
+    use: [
+        {
+            loader: 'file-loader',
+        },
+    ],
+}
+
     return [
+        fileLoader,
+        svgLoader,
         typescriptLoader,
         cssLoader,
     ]
--- src/app/App.tsx
+++ src/app/App.tsx
@@ -5,12 +5,12 @@ import { AppRouter } from "./providers/router";
 import { Navbar } from "widgets/Navbar";
 
 const App = () => {
-  const { theme, toggleTheme } = useTheme();
+  const { theme } = useTheme();
 
   return (
     <div className={classNames("app", {}, [theme])}>
       <Navbar />
-      <button onClick={toggleTheme}>TOGGLE</button>
+
       <AppRouter />
     </div>
   );
