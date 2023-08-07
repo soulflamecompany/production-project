a824bec1de3ce5b007f13cfb05dbd29b96c155f1
Filipp Kadushkin <kadushkin.feniks@gmail.com>
2023-08-07 08:16:20 +0300

    (11) feat: add AppRouter (now we have enum's with names of routes and paths for next reuse) and create config for router

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,9 +2,10 @@ import { Suspense } from "react";
 import { Link, Route, Routes } from "react-router-dom";
 import "./styles/index.scss";
 import { useTheme } from "app/providers/ThemeProvider";
-import { classNames } from "helpers/classNames/classNames";
+import { classNames } from "shared/lib/classNames/classNames";
 import { AboutPage } from "pages/AboutPage";
 import { MainPage } from "pages/MainPage";
+import { AppRouter } from "./providers/router";
 
 const App = () => {
   const { theme, toggleTheme } = useTheme();
@@ -14,13 +15,7 @@ const App = () => {
       <button onClick={toggleTheme}>TOGGLE</button>
       <Link to={"/"}>Главная</Link>
       <Link to={"/about"}>О сайте</Link>
-
-      <Suspense fallback={<div>Loading...</div>}>
-        <Routes>
-          <Route path={"/about"} element={<AboutPage />} />
-          <Route path={"/"} element={<MainPage />} />
-        </Routes>
-      </Suspense>
+      <AppRouter />
     </div>
   );
 };
