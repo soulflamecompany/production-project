e0721eebdc6702bddd00f1dc19fa2ed8bf02e257
Filipp Kadushkin <kadushkin.feniks@gmail.com>
2023-08-07 11:11:10 +0300

    feat: add first widget-Navbar and first UI-component(from UI Kit)--Link(add pair of themes and use them)

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,20 +1,16 @@
-import { Suspense } from "react";
-import { Link, Route, Routes } from "react-router-dom";
 import "./styles/index.scss";
 import { useTheme } from "app/providers/ThemeProvider";
 import { classNames } from "shared/lib/classNames/classNames";
-import { AboutPage } from "pages/AboutPage";
-import { MainPage } from "pages/MainPage";
 import { AppRouter } from "./providers/router";
+import { Navbar } from "widgets/Navbar";
 
 const App = () => {
   const { theme, toggleTheme } = useTheme();
 
   return (
     <div className={classNames("app", {}, [theme])}>
+      <Navbar />
       <button onClick={toggleTheme}>TOGGLE</button>
-      <Link to={"/"}>Главная</Link>
-      <Link to={"/about"}>О сайте</Link>
       <AppRouter />
     </div>
   );
