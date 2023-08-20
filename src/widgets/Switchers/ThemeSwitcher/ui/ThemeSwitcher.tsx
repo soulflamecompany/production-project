0a5ada8b1225a1a27f64ca23790edb8a3b123989
Filipp Kadushkin <kadushkin.feniks@gmail.com>
2023-08-20 16:28:49 +0300

    feat: update Sidebar & Navbar. add some themes for buttons(implement sizes). refactor structure of project(add UI Screenshot test report)

--- src/widgets/Switchers/ThemeSwitcher/ui/ThemeSwitcher.tsx
+++ src/widgets/Switchers/ThemeSwitcher/ui/ThemeSwitcher.tsx
@@ -2,10 +2,10 @@ import { Theme, useTheme } from 'app/providers/ThemeProvider';
 import { classNames } from 'shared/lib/classNames/classNames';
 import LightIcon from 'shared/assets/icons/theme-light.svg';
 import DarkIcon from 'shared/assets/icons/theme-dark.svg';
-import { Button, ThemeButton } from 'shared/ui/Button/Button';
+import { Button, ButtonTheme } from 'shared/ui/Button/Button';
 
 interface ThemeSwitcherProps {
-  className?: string;
+    className?: string;
 }
 
 export const ThemeSwitcher = ({ className }: ThemeSwitcherProps) => {
@@ -13,7 +13,7 @@ export const ThemeSwitcher = ({ className }: ThemeSwitcherProps) => {
 
     return (
         <Button
-            theme={ThemeButton.CLEAR}
+            theme={ButtonTheme.CLEAR}
             className={classNames(undefined, {}, [className])}
             onClick={toggleTheme}
         >
