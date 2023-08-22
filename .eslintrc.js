34ac7c17bc9031200b41103f408b28b539f95963
Filipp Kadushkin <kadushkin.feniks@gmail.com>
2023-08-22 15:35:24 +0300

    feat: add redux store. add first entity. implement examplary component counter

--- .eslintrc.js
+++ .eslintrc.js
@@ -63,6 +63,8 @@ module.exports = {
         'operator-linebreak': 'off',
         'jsx-a11y/no-static-element-interactions': 'off',
         'jsx-a11y/click-events-have-key-events': 'off',
+        'no-param-reassign': 'off',
+        'react/jsx-one-expression-per-line': 'off',
     },
     globals: {
         __IS_DEV__: true,
