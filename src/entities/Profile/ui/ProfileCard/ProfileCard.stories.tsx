4649b523e16ecc4a975b2e82df885e69c2d2ad6c
Filipp Kadushkin <kadushkin.feniks@gmail.com>
2023-09-05 14:59:25 +0300

    feat: add tests for profile module, fix screenshots

---
+++ src/entities/Profile/ui/ProfileCard/ProfileCard.stories.tsx
@@ -0,0 +1,41 @@
+import { ComponentMeta, ComponentStory } from '@storybook/react';
+import { Country } from 'entities/Country';
+import { Currency } from 'entities/Currency';
+import avatar from 'shared/assets/tests/avatar-storybook.jpg';
+import { ProfileCard } from 'entities/Profile';
+
+export default {
+    title: 'entities/ProfileCard',
+    component: ProfileCard,
+    argTypes: {
+        backgroundColor: { control: 'color' },
+    },
+} as ComponentMeta<typeof ProfileCard>;
+
+const Template: ComponentStory<typeof ProfileCard> = (args) => (
+    <ProfileCard {...args} />
+);
+
+export const Primary = Template.bind({});
+Primary.args = {
+    data: {
+        username: 'admin',
+        age: 22,
+        country: Country.Belarus,
+        lastname: 'storybookovich',
+        first: 'storybook',
+        city: 'majorka',
+        currency: Currency.USD,
+        avatar,
+    },
+};
+
+export const withError = Template.bind({});
+withError.args = {
+    error: 'true',
+};
+
+export const Loading = Template.bind({});
+Loading.args = {
+    isLoading: true,
+};
--- src/pages/ProfilePage/ui/ProfilePage.stories.tsx
+++ src/pages/ProfilePage/ui/ProfilePage.stories.tsx
@@ -1,7 +1,10 @@
 import { ComponentStory, ComponentMeta } from '@storybook/react';
 import { Theme } from 'app/providers/ThemeProvider';
+import { Country } from 'entities/Country';
+import { Currency } from 'entities/Currency';
 import { StoreDecorator } from 'shared/config/storybook/StoreDecorator/StoreDecorator';
 import { ThemeDecorator } from 'shared/config/storybook/ThemeDecorator/ThemeDecorator';
+import avatar from 'shared/assets/tests/avatar-storybook.jpg';
 import ProfilePage from './ProfilePage';
 
 export default {
@@ -16,8 +19,39 @@ const Template: ComponentStory<typeof ProfilePage> = () => <ProfilePage />;
 
 export const Normal = Template.bind({});
 Normal.args = {};
-Normal.decorators = [StoreDecorator({})];
+Normal.decorators = [
+    StoreDecorator({
+        profile: {
+            form: {
+                username: 'admin',
+                age: 22,
+                country: Country.Belarus,
+                lastname: 'storybookovich',
+                first: 'storybook',
+                city: 'majorka',
+                currency: Currency.USD,
+                avatar,
+            },
+        },
+    }),
+];
 
 export const Dark = Template.bind({});
 Dark.args = {};
-Dark.decorators = [ThemeDecorator(Theme.DARK), StoreDecorator({})];
+Dark.decorators = [
+    ThemeDecorator(Theme.DARK),
+    StoreDecorator({
+        profile: {
+            form: {
+                username: 'admin',
+                age: 22,
+                country: Country.Belarus,
+                lastname: 'storybookovich',
+                first: 'storybook',
+                city: 'majorka',
+                currency: Currency.USD,
+                avatar,
+            },
+        },
+    }),
+];
